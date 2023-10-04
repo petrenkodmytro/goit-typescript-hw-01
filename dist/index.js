43fdc7a1ea9f0f95667296c0ed8c20888b2d385c
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-10-04 20:07:02 +0300

    add new task

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,24 @@ if (button && input) {
         concatenation(input.value, "hello!");
     });
 }
+var Days;
+(function (Days) {
+    Days[Days["MONDAY"] = 0] = "MONDAY";
+    Days[Days["TUESDAY"] = 1] = "TUESDAY";
+    Days[Days["WEDNESDAY"] = 2] = "WEDNESDAY";
+    Days[Days["THURSDAY"] = 3] = "THURSDAY";
+    Days[Days["FRIDAY"] = 4] = "FRIDAY";
+    Days[Days["SATURDAY"] = 5] = "SATURDAY";
+    Days[Days["SUNDAY"] = 6] = "SUNDAY";
+})(Days || (Days = {}));
+function isWeekend(day) {
+    if (day === Days.SATURDAY || day === Days.SUNDAY) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+const today = Days.FRIDAY;
+console.log(isWeekend(today));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,24 @@ if (button && input) {
     concatenation(input.value, "hello!");
   });
 }
+
+enum Days {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
+
+function isWeekend(day: Days): boolean {
+  if (day === Days.SATURDAY || day === Days.SUNDAY) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const today = Days.FRIDAY;
+console.log(isWeekend(today));
